7529f54c35aa82ed177b57955efa6041cc950d7e
Mark Allibone <mallibone@live.com>
2015-03-30 20:22:39 +0200

    initial commit

---
+++ XamarinFormsNavigation/XamarinFormsNavigation.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using XamarinFormsNavigation.WinPhone.Resources;
+
+namespace XamarinFormsNavigation.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
